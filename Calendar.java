65ed77fc97021e76e22b3e685680117abd797a45
nschumac <schumachernikolai@gmail.com>
2024-01-04 08:38:08 +0100

    push all man whatever

--- Calendar.java
+++ Calendar.java
@@ -9,11 +9,6 @@ public class Calendar {
     static int dayOfWeek = 2; // 1.1.1900 was a Monday
     static int nDaysInMonth = 31; // Number of days in January
 
-    /**
-     * Prints the calendars of all the years in the 20th century. Also prints the
-     * number of Sundays that occured on the first day of the month during this
-     * period.
-     */
     public static void main(String args[]) {
         int calendarYear = Integer.parseInt(args[0]);
 
@@ -29,10 +24,6 @@ public class Calendar {
         }
     }
 
-    // Advances the date (day, month, year) and the day-of-the-week.
-    // If the month changes, sets the number of days in this month.
-    // Side effects: changes the static variables dayOfMonth, month, year,
-    // dayOfWeek, nDaysInMonth.
     private static void advance() {
         Calendar.dayOfWeek++;
         if (Calendar.dayOfWeek > 7) {
@@ -51,16 +42,11 @@ public class Calendar {
         }
     }
 
-    // Returns true if the given year is a leap year, false otherwise.
     private static boolean isLeapYear(int year) {
         return (year % 400 == 0
                 || (year % 100 > 0 && year % 4 == 0));
     }
 
-    // Returns the number of days in the given month and year.
-    // April, June, September, and November have 30 days each.
-    // February has 28 days in a common year, and 29 days in a leap year.
-    // All the other months have 31 days.
     private static int nDaysInMonth(int month, int year) {
         switch (month) {
             case 2: // february
